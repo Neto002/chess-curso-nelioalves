f316441c0881054837ce303866ad8e56f78ad386
Antonio Neto <antoniogfneto92@hotmail.com>
2024-04-13 17:12:21 -0300

    Starting to implement Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,5 @@
 package application;
 
-import board.Position;
 
 public class Main {
     public static void main(String[] args) {
--- src/board/Piece.java
+++ src/board/Piece.java
@@ -1,4 +1,15 @@
 package board;
 
-public class Piece {
+public abstract class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
