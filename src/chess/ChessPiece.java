dfcb677d591e83fbd84764242549e6b9ffc94eff
Antonio Neto <antoniogfneto92@hotmail.com>
2024-04-16 22:28:01 -0300

    Pieces move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import chess.enums.Color;
 
 public abstract class ChessPiece extends Piece {
     private final Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -16,6 +17,18 @@ public abstract class ChessPiece extends Piece {
     protected boolean isThereOponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
+    }
+
+    protected void increaseMoveCount() {
+        moveCount++;
+    }
+
+    protected void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 
     public ChessPosition getChessPosition() {
