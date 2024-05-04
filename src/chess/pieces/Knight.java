fc6677534fccfe36103daa27ee9b988bfbb4d8d0
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-04 14:42:56 -0300

    Knight

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,4 +1,81 @@
 package chess.pieces;
 
-public class Knight {
+import board.Board;
+import board.Position;
+import chess.ChessPiece;
+import chess.enums.Color;
+
+public class Knight extends ChessPiece {
+    public Knight(Board board, Color color) {
+        super(board, color);
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+
+        return p == null || p.getColor() != this.getColor();
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // Left Front
+        p.setValues(this.position.getRow() - 1, this.position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Front Left
+        p.setValues(this.position.getRow() - 2, this.position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Front Right
+        p.setValues(this.position.getRow() - 2, this.position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Right Front
+        p.setValues(this.position.getRow() - 1, this.position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Right Down
+        p.setValues(this.position.getRow() + 1, this.position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Down Right
+        p.setValues(this.position.getRow() + 2, this.position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Down Left
+        p.setValues(this.position.getRow() + 2, this.position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Left Down
+        p.setValues(this.position.getRow() + 1, this.position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
+
+    // Need to override toString since King's letter is also K, having to change to N for the Knight
+    @Override
+    public String toString() {
+        return "N";
+    }
 }
